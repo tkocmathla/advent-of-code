179fb0de1af56f9d93da3f0d03c62f226cf00d6b
Matt Grimm <matt.t.grimm@gmail.com>
2020-12-03 10:39:58 -0700

    better naming, cleanup

--- year2020/aoc-clj/src/aoc_clj/day3.clj
+++ year2020/aoc-clj/src/aoc_clj/day3.clj
@@ -9,22 +9,21 @@
        slurp
        string/split-lines))
 
-(defn p1
-  ([lines] (p1 lines [3 1]))
-  ([lines [dj di]]
-   (let [w (count (first lines))
-         h (count lines)
-         move (fn [[n i j]]
-                [(+ n (if (= \# (get-in lines [i j])) 1 0))
-                 (+ i di)
-                 (mod (+ j dj) w)])]
-     (->> [0 0 0] (iterate move) (drop h) ffirst))))
+(defn p1 [lines [dx dy]]
+  (let [w (count (first lines))
+        h (count lines)
+        move (fn [[n y x]]
+               [(+ n (if (= \# (get-in lines [y x])) 1 0))
+                (+ y dy)
+                (mod (+ x dx) w)])]
+    (->> [0 0 0] (iterate move) (drop h) ffirst)))
 
 (defn p2 [lines]
   (let [slopes [[1 1] [3 1] [5 1] [7 1] [1 2]]]
     (reduce * (map (partial p1 lines) slopes))))
 
 (comment
-  (= 225 (p1 input))
-  (= 1115775000 (p2 input)))
+  (= 225 (p1 input [3 1]))
+  (= 1115775000 (p2 input))
+  )
 
