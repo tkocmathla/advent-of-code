14b8579e8cbbb92bb824af47989abdebe04856d8
Matt Grimm <matt.t.grimm@gmail.com>
2020-12-05 08:14:47 -0700

    cleaner

--- year2020/aoc-clj/src/aoc_clj/day4.clj
+++ year2020/aoc-clj/src/aoc_clj/day4.clj
@@ -10,34 +10,39 @@
        slurp
        string/split-lines))
 
+;; -----------------------------------------------------------------------------
+
+(def valid
+  {"byr" #(<= 1920 (read-string %) 2002)
+   "iyr" #(<= 2010 (read-string %) 2020)
+   "eyr" #(<= 2020 (read-string %) 2030)
+   "hgt" (fn [s]
+           (let [[_ hgt unit] (re-find #"^(\d+)(cm|in)$" s)]
+             (case unit
+               "cm" (<= 150 (read-string hgt) 193)
+               "in" (<= 59 (read-string hgt) 76)
+               false)))
+   "hcl" (partial re-find #"^#[0-9a-f]{6}$")
+   "ecl" #{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"}
+   "pid" (partial re-find #"^[0-9]{9}$")})
+
+(defn parse [lines]
+  (->> (partition-by empty? lines)
+       (filter ffirst)
+       (map #(apply hash-map (string/split (string/join #" " %) #"[: ]")))))
+
+;; -----------------------------------------------------------------------------
+
 (defn p1 [lines]
-  (let [fields #{"byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid" "cid"}]
-    (->> (partition-by empty? lines)
-         (filter ffirst)
-         (map #(set (take-nth 2 (string/split (string/join #" " %) #"[: ]"))))
-         (map (partial set/difference fields))
-         (filter (some-fn empty? (partial = #{"cid"})))
-         count)))
+  (let [valid? (some-fn empty? (partial = #{"cid"}))
+        f #(valid? (set/difference (set (keys valid)) (set (keys %))))]
+    (count (filter f (parse lines)))))
 
 (defn p2 [lines]
-  (let [valid {"byr" #(<= 1920 (read-string %) 2002)
-               "iyr" #(<= 2010 (read-string %) 2020)
-               "eyr" #(<= 2020 (read-string %) 2030)
-               "hgt" (fn [s]
-                       (let [[_ hgt unit] (re-find #"^(\d+)(cm|in)$" s)]
-                         (case unit
-                           "cm" (<= 150 (read-string hgt) 193)
-                           "in" (<= 59 (read-string hgt) 76)
-                           false)))
-               "hcl" (partial re-find #"^#[0-9a-f]{6}$")
-               "ecl" #{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"}
-               "pid" (partial re-find #"^[0-9]{9}$")}]
-    (->> (partition-by empty? lines)
-         (filter ffirst)
-         (map #(apply hash-map (string/split (string/join #" " %) #"[: ]")))
-         (filter #(>= (count %) (count valid)))
-         (filter (fn [m] (every? (fn [k] (when (m k) ((valid k) (m k)))) (keys valid))))
-         count)))
+  (->> (parse lines)
+       (filter #(>= (count %) (count valid)))
+       (filter (fn [m] (every? (fn [k] (when (m k) ((valid k) (m k)))) (keys valid))))
+       count))
 
 (comment
   (= 213 (p1 input))
