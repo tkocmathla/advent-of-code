07e867dc4b4d00bdfc404a42e36f7f4c47eb52e5
Matt Grimm <matt.t.grimm@gmail.com>
2020-12-09 22:42:25 -0700

    2020 day 9 part 2

--- year2020/aoc-clj/src/aoc_clj/day9.clj
+++ year2020/aoc-clj/src/aoc_clj/day9.clj
@@ -17,15 +17,34 @@
        string/split-lines
        (mapv read-string)))
 
-(defn p1 [nums]
-  (->> (drop 25 nums)
+(defn p1 [preamble nums]
+  (->> (drop preamble nums)
        (map-indexed
          (fn [i x]
-           (let [ys (set (subvec nums i (+ i 25)))]
+           (let [ys (set (subvec nums i (+ i preamble)))]
              [x (some (fn [y] (contains? (disj ys y) (- x y))) ys)])))
        (drop-while (comp true? second))
        ffirst))
 
+(defn p2 [preamble nums]
+  (let [target (p1 preamble nums)]
+    (->> (keep-indexed
+           (fn [i x]
+             (->> [[x] x (subvec nums (inc i))]
+                  (iterate
+                    (fn [[xs n [f & r]]]
+                      (let [n' (+ n f)]
+                        (cond
+                          (= n' target) (conj xs f)
+                          (< n' target) [(conj xs f) n' r]))))
+                  (drop-while (comp vector? first))
+                  first))
+           (subvec nums 0 (dec (count nums))))
+         first
+         ((fn [xs] (+ (apply min xs) (apply max xs)))))))
+
 (comment
-  (= 776203571 (p1 input))
-  (= (p2 input)))
+  (= 127 (p1 5 test-input))
+  (= 776203571 (p1 25 input))
+  (= 62 (p2 5 test-input))
+  (= 104800569 (p2 5 input)))
