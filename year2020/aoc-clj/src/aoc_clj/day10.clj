5b4a657e02decb7247cb3b0f915edf691921af1a
Matt Grimm <matt.t.grimm@gmail.com>
2020-12-10 09:21:54 -0700

    better 2020 day 10 part 1

--- year2020/aoc-clj/src/aoc_clj/day10.clj
+++ year2020/aoc-clj/src/aoc_clj/day10.clj
@@ -19,20 +19,19 @@
        (mapv read-string)))
 
 (defn p1 [jolts]
-  (->> [[] [] 0 (set jolts)]
+  (->> [0 1 0 (set jolts)]
        (iterate
          (fn [[ones threes j js]]
            (let [one (js (+ j 1)), two (js (+ j 2)), three (js (+ j 3))]
              (cond
-               one [(conj ones one) threes one (disj js one)]
+               one [(inc ones) threes one (disj js one)]
                two [ones threes two (disj js two)]
-               three [ones (conj threes three) three (disj js three)]))))
+               three [ones (inc threes) three (disj js three)]))))
        (drop-while (comp seq last))
-       first
-       ((fn [[ones threes]] (* (count ones) (inc (count threes)))))))
+       ((fn [[[ones threes]]] (* ones threes)))))
 
 (comment
   (= 35 (p1 test-input))
   (= 2592 (p1 input))
-  (= (p2 test-input))
+  (= 8 (p2 test-input))
   (= (p2 input)))
