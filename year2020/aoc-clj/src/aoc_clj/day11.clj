18ae816ec0f68b6137a9548deea7af3810d321ea
Matt Grimm <matt.t.grimm@gmail.com>
2020-12-11 17:12:02 -0700

    year 2020 day 11 part 2

--- year2020/aoc-clj/src/aoc_clj/day11.clj
+++ year2020/aoc-clj/src/aoc_clj/day11.clj
@@ -24,7 +24,7 @@
           :when (#{\# \L} (get-in grid [y x]))]
       [y x])))
 
-(defn n-neighbors [grid coord]
+(defn adj-neighbors [grid coord]
   (->> [[-1 -1] [-1 0] [-1 1] [0 -1] [0 1] [1 -1] [1 0] [1 1]]
        (map (partial map + coord))
        (remove (partial some neg?))
@@ -32,14 +32,26 @@
        (keep (partial #{\#}))
        count))
 
-(defn p1 [grid]
+(defn all-neighbors [grid coord]
+  (let [valid-coord? (fn [[y x]] (and (<= 0 y (dec (count grid)))
+                                      (<= 0 x (dec (count (first grid))))))]
+    (->> [[-1 -1] [-1 0] [-1 1] [0 -1] [0 1] [1 -1] [1 0] [1 1]]
+         (keep (fn [dydx]
+                   (->> (iterate (partial map + dydx) coord)
+                        (drop 1)
+                        (take-while valid-coord?)
+                        (some (comp #{\# \L} (partial get-in grid)))
+                        #{\#})))
+         count)))
+
+(defn solve [grid min-n neighbor-fn]
   (let [step (fn [g]
                (let [op (fn [seat]
-                          (let [n (n-neighbors g seat)]
+                          (let [n (neighbor-fn g seat)]
                             (or (and (= \L (get-in g seat)) (zero? n) [seat \#])
-                                (and (= \# (get-in g seat)) (>= n 4) [seat \L]))))
+                                (and (= \# (get-in g seat)) (>= n min-n) [seat \L]))))
                      ops (filter identity (map op (valid-seats grid)))
-                     g' (reduce (fn [g [seat c]] (assoc-in g seat c)) g ops)]
+                     g' (reduce (fn [g' [seat c]] (assoc-in g' seat c)) g ops)]
                  (when (not= g g') g')))]
     (->> (iterate step grid)
          (take-while seq)
@@ -47,8 +59,14 @@
          (mapcat (partial filter #{\#}))
          count)))
 
+(defn p1 [grid]
+  (solve grid 4 adj-neighbors))
+
+(defn p2 [grid]
+  (solve grid 5 all-neighbors))
+
 (comment
   (= 37 (p1 test-input))
   (= 2251 (p1 input))
-  (= (p2 test-input))
-  (= (p2 input)))
+  (= 26 (p2 test-input))
+  (= 2019 (p2 input)))
