4eb641bf266f1914acea1830087103cc10d260fe
Matt Grimm <matt.t.grimm@gmail.com>
2020-12-14 19:49:07 -0700

    year 2020 day 14 part 2

--- year2020/aoc-clj/src/aoc_clj/day14.clj
+++ year2020/aoc-clj/src/aoc_clj/day14.clj
@@ -4,25 +4,21 @@
     [clojure.pprint :refer [pprint]]
     [clojure.string :as string]))
 
-(def test-input
-  (->> "day14_test.txt"
+(defn parse [file]
+  (->> file
        io/resource
        slurp
        string/split-lines
        (map #(string/split % #" = "))))
 
-(def input
-  (->> "day14.txt"
-       io/resource
-       slurp
-       string/split-lines
-       (map #(string/split % #" = "))))
+(def test-input (parse "day14_test.txt"))
+(def test-input2 (parse "day14_test2.txt"))
+(def input (parse "day14.txt"))
+
+; ------------------------------------------------------------------------------
 
 (defn to-mask [mask]
-  (->> (reverse mask)
-       (keep-indexed (fn [i c] (case c \0 [i bit-clear] \1 [i bit-set] nil)))
-       flatten
-       (apply hash-map)))
+  (keep-indexed (fn [i c] (case c \0 [i bit-clear] \1 [i bit-set] nil)) (reverse mask)))
 
 (defn to-val [mask x]
   (reduce (fn [x' [i f]] (f x' i)) x mask))
@@ -35,11 +31,36 @@
              [cmds' (to-mask x) mem]
              [cmds' mask (assoc mem (re-find #"\d+" loc) (to-val mask (read-string x)))])))
        (drop-while (comp seq first))
-       first
-       last vals (reduce +)))
+       ((fn [[[_ _ m]]] (reduce + (vals m))))))
+
+; ------------------------------------------------------------------------------
+
+(defn combos [cs]
+  (if (empty? cs)
+    '(())
+    (for [xs (combos (rest cs)), x (first cs)]
+      (cons x xs))))
+
+(defn addrs [mask addr]
+  (let [fix (keep-indexed (fn [i c] (when (= \1 c) i)) (reverse mask))
+        flt (keep-indexed (fn [i c] (when (= \X c) i)) (reverse mask))]
+    (->> (combos (repeat (count flt) [bit-clear bit-set]))
+         (map (fn [combo] (reduce (fn [a [i f]] (f a i)) addr (map vector flt combo))))
+         (map (fn [a] (reduce (fn [a' i] (bit-set a' i)) a fix))))))
+
+(defn p2 [cmds]
+  (->> [cmds nil {}]
+       (iterate
+         (fn [[[[loc x] & cmds'] mask mem]]
+           (let [addr (re-find #"\d+" loc)]
+             (if (= "mask" loc)
+               [cmds' x mem]
+               [cmds' mask (reduce (fn [m a] (assoc m a (read-string x))) mem (addrs mask (read-string addr)))]))))
+       (drop-while (comp seq first))
+       ((fn [[[_ _ m]]] (reduce + (vals m))))))
 
 (comment
   (= 165 (p1 test-input))
   (= 14862056079561 (p1 input))
-  (= (p2 test-input))
-  (= (p2 input)))
+  (= 208 (p2 test-input2))
+  (= 3296185383161 (p2 input)))
