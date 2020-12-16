e97403f8a89b453c11fa3dcd3f20c9b83b77092c
Matt Grimm <matt.t.grimm@gmail.com>
2020-12-15 21:11:20 -0700

    year 2020 day 7 part 2

--- year2020/aoc-clj/src/aoc_clj/day7.clj
+++ year2020/aoc-clj/src/aoc_clj/day7.clj
@@ -5,13 +5,13 @@
     [clojure.set :as set]
     [clojure.string :as string]))
 
-(defonce input
-  (->> "day7.txt"
-       io/resource
-       slurp
-       string/split-lines))
+(defn parse [file]
+  (->> file io/resource slurp string/split-lines))
 
-(defn graph [lines]
+(defonce input (parse "day7.txt"))
+(defonce test-input (parse "day7_test.txt"))
+
+(defn rgraph [lines]
   (reduce
     (fn [m s]
       (let [k (string/join #" " (take 2 (string/split s #" ")))
@@ -28,8 +28,27 @@
        (recur g (into (pop q) conj (set/difference (set (g x)) seen)) (conj seen x))))))
 
 (defn p1 [lines]
-  (dec (count (dfs (graph lines) "shiny gold"))))
+  (dec (count (dfs (rgraph lines) "shiny gold"))))
+
+; ------------------------------------------------------------------------------
+
+(defn graph [lines]
+  (reduce
+    (fn [m s]
+      (let [k (string/join #" " (take 2 (string/split s #" ")))
+            xs (map (comp reverse rest) (re-seq #"(\d+) (\w+ \w+)" s))]
+        (reduce (fn [m' [x n]] (update m' k conj [x (read-string n)])) m xs)))
+    {} lines))
+
+(defn dfs-sum [g n k]
+  (if-let [ks (g k)]
+    (reduce (fn [n' [k' v]] (+ n' v (* v (dfs-sum g 0 k')))) n ks)
+    0))
+
+(defn p2 [lines]
+  (dfs-sum (graph lines) 0 "shiny gold"))
 
 (comment
   (= 248 (p1 input))
-  (= (p2 input)))
+  (= 32 (p2 test-input))
+  (= 57281 (p2 input)))
