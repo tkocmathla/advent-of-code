3525e968652b7b82184f306a1df279d3f6738edb
Matt Grimm <matt.t.grimm@gmail.com>
2021-12-01 15:42:40 -0700

    cleanup for more purty

--- year2021/src/bin/day1.rs
+++ year2021/src/bin/day1.rs
@@ -1,21 +1,20 @@
 use year2021::io;
 
-fn winsum(xs: Vec<i32>) -> i32 {
+fn parse_input(input: String) -> Vec<i32> {
+    input.lines().map(|x| x.parse::<i32>().unwrap()).collect()
+}
+
+fn part1(xs: &Vec<i32>) -> i32 {
     xs.windows(2).map(|win| (win[0] < win[1]) as i32).sum()
 }
 
-fn part1(input: &str) -> i32 {
-    winsum(input.lines().map(|x| x.parse::<i32>().unwrap()).collect())
-}
-
-fn part2(input: &str) -> i32 {
-    let xs: Vec<_> = input.lines().map(|x| x.parse::<i32>().unwrap()).collect();
-    winsum(xs.windows(3).map(|win| win.iter().sum()).collect())
+fn part2(xs: &Vec<i32>) -> i32 {
+    part1(&xs.windows(3).map(|win| win.iter().sum()).collect())
 }
 
 fn main() {
-    let input = io::read_input(1);
+    let xs = parse_input(io::read_input(1));
 
-    assert_eq!(1462, part1(&input));
-    assert_eq!(1497, part2(&input));
+    assert_eq!(1462, part1(&xs));
+    assert_eq!(1497, part2(&xs));
 }
