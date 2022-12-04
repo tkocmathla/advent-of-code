8fc001095cb42d7f4bbdd165d891e388f656e77f
Matt Grimm <matt.t.grimm@gmail.com>
2022-12-04 08:29:28 -0700

    format

--- year2022/bin/day4.ml
+++ year2022/bin/day4.ml
@@ -2,8 +2,7 @@ let contains1 a b c d = c >= a && d <= b
 let contains2 a b c d = (c <= a && d >= a) || (c <= b && d >= b)
 
 let step f acc s =
-  Scanf.sscanf s "%d-%d,%d-%d" (fun a b c d ->
-      acc + Bool.to_int (f a b c d || f c d a b))
+  Scanf.sscanf s "%d-%d,%d-%d" (fun a b c d -> acc + Bool.to_int (f a b c d || f c d a b))
 
 let solve f seq = Seq.fold_left (step f) 0 seq
 
