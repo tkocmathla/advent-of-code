0664042f4f6b726ac56e021a51d4c76455544a0d
Matt Grimm <matt.t.grimm@gmail.com>
2022-12-09 14:54:12 -0700

    generalized day 9 (wip for part 2)

--- year2022/bin/day9.ml
+++ year2022/bin/day9.ml
@@ -5,32 +5,39 @@ let unpack_cmds cmds =
   Seq.flat_map (fun cmd -> Scanf.sscanf cmd "%s %d" parse) cmds
   |> List.of_seq
 
-let follow hx hy (tx, ty) =
+let follow (hx, hy) (tx, ty) =
   let xdist = hx - tx in
   let ydist = hy - ty in
   if Int.abs ydist > 1 then (hx, hy + (if ydist < 0 then 1 else -1))
   else if Int.abs xdist > 1 then (hx + (if xdist < 0 then 1 else -1), hy)
   else (tx, ty)
 
-let step ((hx, hy), tail, visited, cmds) =
-  let (newh, newt, newcmds) =
-    match cmds with
-    | "U" :: rest -> ((hx, hy - 1), follow hx (hy - 1) tail, rest)
-    | "D" :: rest -> ((hx, hy + 1), follow hx (hy + 1) tail, rest)
-    | "L" :: rest -> ((hx - 1, hy), follow (hx - 1) hy tail, rest)
-    | "R" :: rest -> ((hx + 1, hy), follow (hx + 1) hy tail, rest)
-    | _ -> failwith "derp"
-  in
-  (newh, newt, PointSet.add newt visited, newcmds)
+let rec update_step head = function
+  | [] -> []
+  | knot :: [] -> [follow head knot]
+  | knot :: knots ->
+      let next_head = follow head knot in
+      next_head :: update_step next_head knots
 
-let part1 seq =
-  Seq.iterate step ((0, 0), (0, 0), PointSet.add (0, 0) PointSet.empty, unpack_cmds seq)
-  |> Seq.drop_while (fun (_, _, _, cmds) -> match cmds with [] -> false | _ -> true)
-  |> Seq.uncons
-  |> function | None -> 0 | Some ((_, _, set, _), _) -> PointSet.cardinal set
+let step (knots, cmds) =
+  let (hx, hy) = List.hd knots in
+  match cmds with
+  | "U" :: cmds -> let hd = (hx, hy - 1) in hd :: update_step hd (List.tl knots), cmds
+  | "D" :: cmds -> let hd = (hx, hy + 1) in hd :: update_step hd (List.tl knots), cmds
+  | "L" :: cmds -> let hd = (hx - 1, hy) in hd :: update_step hd (List.tl knots), cmds
+  | "R" :: cmds -> let hd = (hx + 1, hy) in hd :: update_step hd (List.tl knots), cmds
+  | _ -> (knots, cmds)
+
+let solve n seq =
+  let knots = Seq.take n (Seq.repeat (0, 0)) |> List.of_seq in
+  Seq.iterate step (knots, unpack_cmds seq)
+  |> Seq.take_while (fun (_, cmds) -> match cmds with [] -> false | _ -> true)
+  |> Seq.fold_left (fun set (knots, _) -> PointSet.add (List.nth knots (n - 1)) set) PointSet.empty
+  |> PointSet.cardinal |> ((+) 1) (* FIXME WHY!? *)
 
 let () =
   print_endline "";
   let seq = Arg.read_arg "data/day9.txt" |> Array.to_seq in
 
-  part1 seq |> string_of_int |> print_endline;
+  solve 2 seq |> string_of_int |> print_endline; (* 6236 *)
+  solve 10 seq |> string_of_int |> print_endline
