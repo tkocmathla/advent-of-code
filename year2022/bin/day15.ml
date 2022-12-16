64e6f8b6e08f77de5a6762c656a6cb1634f57384
Matt Grimm <matt.t.grimm@gmail.com>
2022-12-15 21:37:48 -0700

    year 2022 day 15 part 2

--- year2022/bin/day15.ml
+++ year2022/bin/day15.ml
@@ -1,9 +1,11 @@
+open Year2022.Common
+
 let parse_input lines =
   let to_pairs x1 y1 x2 y2 = ((x1, y1), (x2, y2)) in
   let parse s = Scanf.sscanf s "Sensor at x=%d, y=%d: closest beacon is at x=%d, y=%d" to_pairs in
   List.fold_left (fun l s -> (parse s) :: l) [] lines
 
-let part1 y sensors =
+let coverages y sensors =
   List.fold_left
     (fun l ((sx, sy), (bx, by)) ->
       let dist = Int.abs (sx - bx) + Int.abs (sy - by) in
@@ -13,13 +15,40 @@ let part1 y sensors =
         (sx - dx, sx + dx) :: l
       else l)
     [] sensors
-  |> List.fold_left
-       (fun (min, max) (rmin, rmax) -> (Int.min min rmin, Int.max max rmax))
-       (Int.max_int, Int.min_int)
+
+let part1 y sensors =
+  coverages y sensors
+  |> List.fold_left (fun (min, max) (x1, x2) -> Int.min min x1, Int.max max x2) (Int.max_int, Int.min_int)
   |> fun (min, max) -> max - min
+
+let coalesce ranges =
+  let rec step min1 max1 = function
+    | (min2, max2) :: rest ->
+        if max1 <> Int.min_int && max1 < min2 then
+          (min1, max1) :: step min2 (Int.max max1 max2) rest
+        else
+          step (Int.min min1 min2) (Int.max max1 max2) rest
+    | [] -> [(min1, max1)]
+  in
+  step Int.max_int Int.min_int ranges
+
+let part2 extent sensors =
+  Seq.iterate succ 0 |> Seq.take extent
+  |> Seq.find_map
+       (fun y ->
+         coverages y sensors |> List.sort compare |> coalesce |> List.to_seq
+         |> partition 2 1
+         |> Seq.find_map
+              (fun p ->
+                match List.of_seq p with
+                | [(_, x1); (x2, _)] -> if x1 < x2 then Some (x1 + 1) else None
+                | _ -> None)
+         |> function Some x -> Some (x * extent + y) | _ -> None)
+  |> function Some ans -> ans | _ -> failwith ""
 
 let () =
   print_endline "";
   let sensors = Arg.read_arg "data/day15.txt" |> Array.to_list |> parse_input in
 
-  part1 2000000 sensors |> string_of_int |> print_endline (* 4876693 *)
+  part1 2000000 sensors |> string_of_int |> print_endline; (* 4876693 *)
+  part2 4000000 sensors |> string_of_int |> print_endline (* 11645454855041 *)
--- year2022/lib/common.ml
+++ year2022/lib/common.ml
@@ -10,8 +10,15 @@ module PointMap = Map.Make (struct
   let compare = compare
 end)
 
-let rcompare a b = compare b a
+(* Converts the number represented by ch into an integer *)
 let int_from_char ch = int_of_char ch - int_of_char '0'
+
+(* Reverse comparator *)
+let rcompare a b = compare b a
+
+let ( -- ) i j =
+  let rec aux n acc = if n < i then acc else aux (n - 1) (n :: acc) in
+  aux j []
 
 let range_from start stop =
   if start > stop then []
@@ -19,3 +26,14 @@ let range_from start stop =
 
 (* Returns a list of ints from 0 to n when n is > 0, or an empty list otherwise *)
 let range n = if n < 0 then [] else range_from 0 n
+
+(* https://clojuredocs.org/clojure.core/partition *)
+let rec partition n step coll =
+  let p = Seq.take n coll in
+  if n = Seq.length p then Seq.cons p (partition n step (Seq.drop step coll))
+  else Seq.empty
+
+(* https://clojuredocs.org/clojure.core/partition-all *)
+let rec partition_all n step coll =
+  if Seq.is_empty coll then Seq.empty
+  else Seq.cons (Seq.take n coll) (partition_all n step (Seq.drop step coll))
