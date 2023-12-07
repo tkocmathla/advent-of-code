58cb5bf961c9c3e18bbe4bebec1f9ebd6e3ca7c9
Matt Grimm <matt.t.grimm@gmail.com>
2023-12-07 11:12:29 -0700

    probably cleaner

--- year2023/aoc-clj/src/aoc_clj/day7.clj
+++ year2023/aoc-clj/src/aoc_clj/day7.clj
@@ -9,50 +9,48 @@
 (defonce input (->> "day7.txt" io/resource slurp))
 
 (defn number [xs] (into {} (map vector xs (range))))
-(def hand-cardinality (number [:five-of-a-kind :four-of-a-kind :full-house :three-of-a-kind :two-pair :one-pair :high-card]))
+(def hand-cardinality (number [:five-of-kind :four-of-kind :full-house :three-of-kind :two-pair :one-pair :high-card]))
 (def card-cardinality (number [\A \K \Q \J \T \9 \8 \7 \6 \5 \4 \3 \2]))
 
-(defn upgrade-j [s]
-  (let [s' (remove #{\J} s)]
-    (if (empty? s')
+(defn upgrade-j [cards]
+  (let [cards' (remove #{\J} cards)
+        best-card #(str (first (last (sort-by (juxt val (comp card-cardinality key)) (frequencies %)))))]
+    (if (empty? cards')
       "AAAAA"
-      (let [[card _] (last (sort-by (juxt val (comp card-cardinality key)) (frequencies s')))]
-        (string/replace s #"J" (str card))))))
+      (string/replace cards #"J" (best-card cards')))))
 
-(defn to-hand [s upgrade-j?]
-  (let [s (cond-> s upgrade-j? upgrade-j)
-        freq (vals (frequencies s))
+(defn to-hand [upgrade-j? s]
+  (let [freq (vals (frequencies (cond-> s upgrade-j? upgrade-j)))
         pairs #(count (filter #{2} %))]
     (cond
-      (= 1 (count (set s))) :five-of-a-kind
-      (some #{4} freq) :four-of-a-kind
+      (= 1 (count freq)) :five-of-kind
+      (some #{4} freq) :four-of-kind
       (and (some #{3} freq) (= 1 (pairs freq))) :full-house
-      (and (some #{3} freq) (some #{1} freq)) :three-of-a-kind
+      (and (some #{3} freq) (some #{1} freq)) :three-of-kind
       (= 2 (pairs freq)) :two-pair
       (and (= 1 (pairs freq)) (= 3 (count (filter #{1} freq)))) :one-pair
       (= 5 (count freq)) :high-card)))
 
 (defn parse-hand [upgrade-j? s]
   (let [[hand bid] (string/split s #" ")]
-    [hand (to-hand hand upgrade-j?) (str-int bid)]))
+    [hand (to-hand upgrade-j? hand) (str-int bid)]))
 
 (defn sort-hands [upgrade-j? [s hand _]]
   (let [card-cardinality' (cond-> card-cardinality upgrade-j? (assoc \J Integer/MAX_VALUE))]
     (vec (cons (hand-cardinality hand) (mapv card-cardinality' s)))))
 
-(defn solve [s & [upgrade-j?]]
+(defn solve [upgrade-j? s]
   (let [hands (map (partial parse-hand upgrade-j?) (string/split-lines s))]
     (->> (sort-by (partial sort-hands upgrade-j?) hands)
          reverse
          (reduce (fn [[i sum] [_ _ bid]] [(inc i) (+ sum (* i bid))]) [1 0])
          second)))
 
-(defn p1 [s] (solve s))
-(defn p2 [s] (solve s true))
+(defn p1 [s] (solve false s))
+(defn p2 [s] (solve true s))
 
-(comment
-  (= 6440 (p1 test-input))
-  (= 247823654 (p1 input))
+(= 6440 (p1 test-input))
+(= 247823654 (p1 input))
 
-  (= 5905 (p2 test-input))
-  (= 245461700 (p2 input)))
+(= 5905 (p2 test-input))
+(= 245461700 (p2 input))
