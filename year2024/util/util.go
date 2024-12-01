26000a7d7030a9066096612280570c3cc57f9da2
tkocmathla <matt.t.grimm@gmail.com>
2024-12-01 12:10:35 -0700

    make TimeFunc generic on the return type

--- year2024/util/util.go
+++ year2024/util/util.go
@@ -16,13 +16,13 @@ func Try[T any](value T, e error) T {
 }
 
 // DayPartFunc is a function to solve a day part that takes the path to the input file and returns the solution.
-type DayPartFunc func(string) int
+type DayPartFunc[T any] func(string) T
 
 // TimeFunc runs the given function and prints the result formatted with the function name, result, and elapsed time.
-func TimeFunc(f DayPartFunc, arg string) {
+func TimeFunc[T any](f DayPartFunc[T], arg string) {
 	rfn := runtime.FuncForPC(reflect.ValueOf(f).Pointer())
 	now := time.Now()
 	ans := f(arg)
 	elapsed := time.Since(now)
-	fmt.Printf("%s: %d (%s)\n", rfn.Name(), ans, elapsed)
+	fmt.Printf("%s: %v (%s)\n", rfn.Name(), ans, elapsed)
 }
