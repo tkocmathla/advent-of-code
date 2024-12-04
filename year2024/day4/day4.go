17f88d5e957dad4f7268fbfe35f74cfc6b83e035
tkocmathla <matt.t.grimm@gmail.com>
2024-12-04 13:10:00 -0700

    clean up

--- year2024/day4/day4.go
+++ year2024/day4/day4.go
@@ -17,14 +17,13 @@ func is_xmas(grid []string, loc aoc.Point, dir aoc.Point) bool {
 }
 
 func is_x_mas(grid []string, loc aoc.Point) bool {
-	in_bounds := loc.Y > 0 && loc.X > 0 && loc.Y < len(grid)-1 && loc.X < len(grid[loc.Y])-1
-	if !in_bounds {
-		return false
+	if loc.Y > 0 && loc.X > 0 && loc.Y < len(grid)-1 && loc.X < len(grid[loc.Y])-1 {
+		has_a := grid[loc.Y][loc.X] == 'A'
+		has_mas1 := (grid[loc.Y-1][loc.X-1] == 'M' && grid[loc.Y+1][loc.X+1] == 'S') || (grid[loc.Y-1][loc.X-1] == 'S' && grid[loc.Y+1][loc.X+1] == 'M')
+		has_mas2 := (grid[loc.Y-1][loc.X+1] == 'M' && grid[loc.Y+1][loc.X-1] == 'S') || (grid[loc.Y-1][loc.X+1] == 'S' && grid[loc.Y+1][loc.X-1] == 'M')
+		return has_a && has_mas1 && has_mas2
 	}
-	has_a := grid[loc.Y][loc.X] == 'A'
-	has_mas1 := (grid[loc.Y-1][loc.X-1] == 'M' && grid[loc.Y+1][loc.X+1] == 'S') || (grid[loc.Y-1][loc.X-1] == 'S' && grid[loc.Y+1][loc.X+1] == 'M')
-	has_mas2 := (grid[loc.Y-1][loc.X+1] == 'M' && grid[loc.Y+1][loc.X-1] == 'S') || (grid[loc.Y-1][loc.X+1] == 'S' && grid[loc.Y+1][loc.X-1] == 'M')
-	return has_a && has_mas1 && has_mas2
+	return false
 }
 
 func part1(input string) int {
