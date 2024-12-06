4dcf8642c0afd9fee5bb3a036a9f2e1b8ea3a9dc
tkocmathla <matt.t.grimm@gmail.com>
2024-12-06 15:49:21 -0700

    better naming

--- year2024/day6/day6.go
+++ year2024/day6/day6.go
@@ -15,13 +15,13 @@ type Guard struct {
 
 var start = aoc.Point{Y: 45, X: 42}
 
-func in_bounds(grid *[]string, loc aoc.Point) bool {
+func valid(grid *[]string, loc aoc.Point) bool {
 	return loc.Y >= 0 && loc.X >= 0 && loc.Y < len(*grid) && loc.X < len((*grid)[0])
 }
 
 func step(grid *[]string, guard *Guard) {
 	next := aoc.Point{Y: guard.loc.Y + guard.dir.Y, X: guard.loc.X + guard.dir.X}
-	if in_bounds(grid, next) && (*grid)[next.Y][next.X] == '#' {
+	if valid(grid, next) && (*grid)[next.Y][next.X] == '#' {
 		switch guard.dir {
 		case aoc.N:
 			guard.dir = aoc.E
@@ -41,18 +41,13 @@ func step(grid *[]string, guard *Guard) {
 func walk(grid []string, loc aoc.Point) map[aoc.Point]bool {
 	guard := Guard{loc: loc, dir: aoc.N}
 	seen := make(map[aoc.Point]bool)
-	for seen[guard.loc] = true; in_bounds(&grid, guard.loc); step(&grid, &guard) {
+	for seen[guard.loc] = true; valid(&grid, guard.loc); step(&grid, &guard) {
 		seen[guard.loc] = true
 	}
 	return seen
 }
 
-func Part1(input string) int {
-	grid := s.Fields(string(aoc.Try(os.ReadFile(input))))
-	return len(walk(grid, start))
-}
-
-func hasCycle(orig_grid *[]string, x, y int) bool {
+func cyclic(orig_grid *[]string, x, y int) bool {
 	if (aoc.Point{Y: y, X: x} == start) || (*orig_grid)[y][x] == '#' {
 		return false
 	}
@@ -62,13 +57,18 @@ func hasCycle(orig_grid *[]string, x, y int) bool {
 
 	guard := Guard{loc: start, dir: aoc.N}
 	seen := make(map[Guard]bool)
-	for ; in_bounds(&local_grid, guard.loc); step(&local_grid, &guard) {
+	for ; valid(&local_grid, guard.loc); step(&local_grid, &guard) {
 		if _, has := seen[guard]; has {
 			return true
 		}
 		seen[guard] = true
 	}
 	return false
+}
+
+func Part1(input string) int {
+	grid := s.Fields(string(aoc.Try(os.ReadFile(input))))
+	return len(walk(grid, start))
 }
 
 func Part2(input string) int32 {
@@ -80,7 +80,7 @@ func Part2(input string) int32 {
 		wg.Add(1)
 		go func(loc aoc.Point) {
 			defer wg.Done()
-			if hasCycle(&grid, loc.X, loc.Y) {
+			if cyclic(&grid, loc.X, loc.Y) {
 				atomic.AddInt32(&loops, 1)
 			}
 		}(loc)
