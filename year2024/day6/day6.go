906597eb78478fa6c5ad83b0dba8a38475f52831
tkocmathla <matt.t.grimm@gmail.com>
2024-12-06 14:35:30 -0700

    bestest

--- year2024/day6/day6.go
+++ year2024/day6/day6.go
@@ -13,13 +13,15 @@ type Guard struct {
 	dir aoc.Point
 }
 
-func in_bounds(grid []string, loc aoc.Point) bool {
-	return loc.Y >= 0 && loc.X >= 0 && loc.Y < len(grid) && loc.X < len(grid[0])
+var start = aoc.Point{Y: 45, X: 42}
+
+func in_bounds(grid *[]string, loc aoc.Point) bool {
+	return loc.Y >= 0 && loc.X >= 0 && loc.Y < len(*grid) && loc.X < len((*grid)[0])
 }
 
-func step(grid []string, guard *Guard) {
+func step(grid *[]string, guard *Guard) {
 	next := aoc.Point{Y: guard.loc.Y + guard.dir.Y, X: guard.loc.X + guard.dir.X}
-	if in_bounds(grid, next) && grid[next.Y][next.X] == '#' {
+	if in_bounds(grid, next) && (*grid)[next.Y][next.X] == '#' {
 		switch guard.dir {
 		case aoc.N:
 			guard.dir = aoc.E
@@ -36,30 +38,31 @@ func step(grid []string, guard *Guard) {
 	}
 }
 
-var start = aoc.Point{Y: 45, X: 42}
+func walk(grid []string, loc aoc.Point) map[aoc.Point]bool {
+	guard := Guard{loc: loc, dir: aoc.N}
+	seen := make(map[aoc.Point]bool)
+	for seen[guard.loc] = true; in_bounds(&grid, guard.loc); step(&grid, &guard) {
+		seen[guard.loc] = true
+	}
+	return seen
+}
 
 func Part1(input string) int {
 	grid := s.Fields(string(aoc.Try(os.ReadFile(input))))
-	guard := Guard{loc: start, dir: aoc.N}
-	seen := make(map[aoc.Point]bool)
-	for seen[guard.loc] = true; in_bounds(grid, guard.loc); step(grid, &guard) {
-		seen[guard.loc] = true
-	}
-	return len(seen)
+	return len(walk(grid, start))
 }
 
 func hasCycle(orig_grid *[]string, x, y int) bool {
 	if (aoc.Point{Y: y, X: x} == start) || (*orig_grid)[y][x] == '#' {
 		return false
 	}
-
 	local_grid := make([]string, len(*orig_grid))
 	copy(local_grid, *orig_grid)
 	local_grid[y] = local_grid[y][:x] + "#" + local_grid[y][x+1:]
 
 	guard := Guard{loc: start, dir: aoc.N}
 	seen := make(map[Guard]bool)
-	for ; in_bounds(local_grid, guard.loc); step(local_grid, &guard) {
+	for ; in_bounds(&local_grid, guard.loc); step(&local_grid, &guard) {
 		if _, has := seen[guard]; has {
 			return true
 		}
@@ -72,16 +75,15 @@ func Part2(input string) int32 {
 	var loops int32
 	var wg sync.WaitGroup
 	grid := s.Fields(string(aoc.Try(os.ReadFile(input))))
-	for y := range grid {
-		for x := range grid[y] {
-			wg.Add(1)
-			go func(x, y int) {
-				defer wg.Done()
-				if hasCycle(&grid, x, y) {
-					atomic.AddInt32(&loops, 1)
-				}
-			}(x, y)
-		}
+	seen := walk(grid, start)
+	for loc := range seen {
+		wg.Add(1)
+		go func(loc aoc.Point) {
+			defer wg.Done()
+			if hasCycle(&grid, loc.X, loc.Y) {
+				atomic.AddInt32(&loops, 1)
+			}
+		}(loc)
 	}
 	wg.Wait()
 	return loops
--- year2024/main_test.go
+++ year2024/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"aoc/day4"
+	"aoc/day6"
 	"testing"
 )
 
@@ -14,5 +15,17 @@ func BenchmarkDay4Part1(b *testing.B) {
 func BenchmarkDay4Part2(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		day4.Part2("data/day4.txt")
+	}
+}
+
+func BenchmarkDay6Part1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		day6.Part1("data/day6.txt")
+	}
+}
+
+func BenchmarkDay6Part2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		day6.Part2("data/day6.txt")
 	}
 }
