bf027897e732538daac5bb2a7ec90fbd1c452a34
tkocmathla <matt.t.grimm@gmail.com>
2024-12-09 13:05:16 -0700

    omg slice > dll

--- year2024/day9/day9.go
+++ year2024/day9/day9.go
@@ -2,73 +2,60 @@ package day9
 
 import (
 	aoc "aoc/util"
-	"container/list"
 	"os"
 	"strconv"
 	s "strings"
 )
 
-type Block struct {
-	i   int // block index
-	fid int // file id
-}
-
 var FreeId = -1
 
-func parse(input string) *list.List {
+func parse(input string) []int {
 	data := s.TrimSpace(string(aoc.Try(os.ReadFile(input))))
-	disk := list.New()
-	index := 0
+	var disk []int
 	for i, c := range data {
 		size := aoc.Try(strconv.Atoi(string(c)))
 		for j := 0; j < size; j++ {
 			if i%2 == 0 {
-				disk.PushBack(&Block{i: index, fid: i / 2})
+				disk = append(disk, i/2)
 			} else {
-				disk.PushBack(&Block{i: index, fid: FreeId})
+				disk = append(disk, FreeId)
 			}
-			index += 1
 		}
 	}
 	return disk
 }
 
-func find_free(disk *list.List, start *list.Element) *list.Element {
-	for e := start; e != nil; e = e.Next() {
-		if e.Value.(*Block).fid == FreeId {
-			return e
+func find_free(disk *[]int, start int) int {
+	for i, fid := range *disk {
+		if fid == FreeId {
+			return i
 		}
 	}
-	return nil
+	return -1
 }
 
 func Part1(input string) int {
 	disk := parse(input)
-	free := find_free(disk, disk.Front())
-	for e := disk.Back(); e != nil; e = e.Prev() {
-		blk := e.Value.(*Block)
-		if blk.fid == FreeId {
+	free := find_free(&disk, 0)
+	for i := len(disk) - 1; i >= 0; i-- {
+		if disk[i] == FreeId {
 			continue
 		}
-		if free = find_free(disk, free); free.Value.(*Block).i > blk.i {
+		if free = find_free(&disk, free); free > i {
 			break
 		}
-		free.Value.(*Block).fid = blk.fid
-		blk.fid = FreeId
+		disk[free], disk[i] = disk[i], disk[free]
 	}
 
 	sum := 0
-	for e := disk.Front(); e != nil && e.Value.(*Block).fid != FreeId; e = e.Next() {
-		sum += e.Value.(*Block).fid * e.Value.(*Block).i
+	for i, fid := range disk {
+		if fid != FreeId {
+			sum += i * fid
+		}
 	}
-
 	return sum
 }
 
-//func Part2(input string) int {
-//}
-
 func Solve() {
 	aoc.AssertEq(aoc.TimeFunc(Part1, "data/day9.txt"), 6356833654075)
-	//aoc.AssertEq(aoc.TimeFunc(Part2, "data/day9.txt"), 0)
 }
